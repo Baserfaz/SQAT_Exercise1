786c09450c401027f3f08dea37ee21fdca57f3db
somename <someemail>
2016-09-23 16:00:50 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -48,7 +48,11 @@ public class BowlingGame {
 			
 			if(f.isStrike()) {
 				myScore += f.getFirstThrow() + frames.get(i + 1).score();
-				if(!f.isSpare()) continue; // if the subsequential frame is a spare... calculate spare too and don't continue.
+				
+				// if the subsequential frame is a spare ...
+				// -> calculate spare too and don't continue.
+				
+				if(!f.isSpare()) continue; 
 			}
 			
 			if(f.isSpare()) {
